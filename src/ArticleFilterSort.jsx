55aa5f2d1b08668c3e9f0865ab100cab29ba73fd
Megrocky <megrocky63@gmail.com>
2025-02-19 10:14:46 +0000

    add filter options

---
+++ src/ArticleFilterSort.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+function ArticleFiterSort({ setSearchParams, searchParams }) {
+  const [selectedOrder, setSelectedOrder] = useState("Decending");
+  const [selectedSortBy, setSelectedSortBy] = useState("Date Posted");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const orderDirection = selectedOrder === "Ascending" ? "asc" : "desc";
+    let sortColumn;
+    switch (selectedSortBy) {
+      case "Date Posted":
+        sortColumn = "created_at";
+        break;
+      case "Comment Count":
+        sortColumn = "comment_count";
+        break;
+      case "Votes":
+        sortColumn = "votes";
+        break;
+    }
+    const newParams = new URLSearchParams(searchParams);
+    newParams.set("order", orderDirection);
+    newParams.set("sorted_by", sortColumn);
+    setSearchParams(newParams);
+  };
+
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <select
+          name="order"
+          onChange={(event) => setSelectedOrder(event.target.value)}
+          value={selectedOrder}
+        >
+          <option>Ascending</option>
+          <option>Decending</option>
+        </select>
+        <select
+          name="sorted_by"
+          onChange={(event) => setSelectedSortBy(event.target.value)}
+          value={selectedSortBy}
+        >
+          <option>Date Posted</option>
+          <option>Comment Count</option>
+          <option>Votes</option>
+        </select>
+        <button type="submit">Apply Sort</button>
+      </form>
+    </section>
+  );
+}
+export default ArticleFiterSort;
